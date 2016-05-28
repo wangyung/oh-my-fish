5f9760f91364d9b1140ce5f4398dc0df1435485b
Derek Stavis <dekestavis@gmail.com>
2016-05-28 17:13:21 -0300

    list: refactor globs for fish 2.3 compatibility

--- pkg/omf/functions/cli/omf.cli.list.fish
+++ pkg/omf/functions/cli/omf.cli.list.fish
@@ -1,14 +1,3 @@
-function omf.cli.list -a type
-  test -z "$type"; and set type '--installed'
-
-  if contains -- $type '--available' '-a' '--database' '-d' '--installed' '-i'
-    omf.packages.list $type | column
-  else
-    echo (omf::err)"Invalid arguments"(omf::off)
-    echo 'Usage: omf list [ --available | -a ]'
-    echo '       omf list [ --installed | -i ]'
-    echo '       omf list [ --database  | -d ]'
-    return $OMF_INVALID_ARG
-  end
-
+function omf.cli.list
+  omf.packages.list $argv | column
 end
